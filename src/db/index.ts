f45f491712025130950b26d1a91c6fbbcff16dd3
daniel-lou18 <danielderudder@hotmail.com>
2024-12-17 16:40:42 +0100

    Add db path to env file

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,18 @@
+import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "./schema";
 import path from "path";
 
-const sqlite = new Database(path.join(__dirname, "pairs.db"));
+dotenv.config();
+
+const DB_PATH = process.env.DB_PATH;
+
+if (!DB_PATH) {
+  throw new Error("DB_PATH is not defined in .env file");
+}
+
+const sqlite = new Database(path.join(__dirname, DB_PATH));
 export const db = drizzle({ client: sqlite, schema });
 
 export default db;
--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ import { eq } from "drizzle-orm";
 import { usersTable } from "./db/schema";
 
 async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: "John",
-    age: 30,
-    email: "john@example.com",
-  };
-  await db.insert(usersTable).values(user);
-  console.log("New user created!");
+  // const user: typeof usersTable.$inferInsert = {
+  //   name: "John",
+  //   age: 30,
+  //   email: "john@example.com",
+  // };
+  // await db.insert(usersTable).values(user);
+  // console.log("New user created!");
   const users = await db.select().from(usersTable);
   console.log("Getting all users from the database: ", users);
 
